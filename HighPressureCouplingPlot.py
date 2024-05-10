66e3aaa6b53e633692edd3d1ea2df76b91a5879f
alvarodelrio22 <delriomonleona@gmail.com>
2024-05-10 22:56:14 +0800

    Changes 10/05/2024

--- HighPressureCouplingPlot.py
+++ HighPressureCouplingPlot.py
@@ -1,6 +1,8 @@
+import numpy as np, warnings, time
+
 from Solvers.HighPressureCouplingSolver import hpCoupling
-import numpy as np, warnings
 from Components.MapPlotFunction import componentPlot
+from alive_progress import alive_bar
 
 warnings.filterwarnings("ignore")
 
@@ -11,24 +13,29 @@ N_HPC = np.linspace(0.45,1.08,Num_points)
 var = ["mC", "piC", "etaC", "mT", "piT", "etaT"]
 map = {}
 
-for x in var:
-    map[x] = np.zeros([Num_points, Num_points])
+with alive_bar(len(beta_HPC)*len(N_HPC)) as bar:
 
-for i in range(len(beta_HPC)):
+    for x in var:
+        map[x] = np.zeros([Num_points, Num_points])
 
-    for j in range(len(N_HPC)):
+    for i in range(len(beta_HPC)):
 
-        print("Iteration: " + str(i*len(beta_HPC) + (j+1)) + "/" + str(len(beta_HPC)*len(N_HPC)))
+        for j in range(len(N_HPC)):
 
-        m_25, p3t_p25t, eta_HPC, m_41, p45t_p41t, eta_HPT = hpCoupling(beta_HPC[i],N_HPC[j],100,0.9,True)
+            print("Iteration: " + str(i*len(beta_HPC) + (j+1)) + "/" + str(len(beta_HPC)*len(N_HPC)))
 
-        map["mC"][i,j] = m_25
-        map["piC"][i,j] = p3t_p25t
-        map["etaC"][i,j] = eta_HPC
+            m_25, p3t_p25t, eta_HPC, m_41, p45t_p41t, eta_HPT = hpCoupling(beta_HPC[i],N_HPC[j],100,0.9,True)
 
-        map["mT"][i,j] = m_41
-        map["piT"][i,j] = 1/p45t_p41t
-        map["etaT"][i,j] = eta_HPT
+            map["mC"][i,j] = m_25
+            map["piC"][i,j] = p3t_p25t
+            map["etaC"][i,j] = eta_HPC
+
+            map["mT"][i,j] = m_41
+            map["piT"][i,j] = 1/p45t_p41t
+            map["etaT"][i,j] = eta_HPT
+
+            time.sleep(5e-4)
+            bar()
 
 pltC = componentPlot("HPC",False)
 coupled_mapC = pltC.contourf(map["mC"],map["piC"],map["etaC"],np.linspace(0.5,0.9,100),cmap = 'jet')
